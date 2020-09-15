a8177d60391b5ab18f840e4294c01e2f92d3d311
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-15 23:12:20 +0300

    prevent reusing score more than once

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
+    let ruleRowStatus =
+      this.props.score === undefined ? "RuleRow-active" : "RuleRow-disabled";
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr
+        className={`RuleRow ${ruleRowStatus}`}
+        onClick={this.props.score === undefined ? this.props.doScore : null}
+      >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
-    )
+    );
   }
 }
 
