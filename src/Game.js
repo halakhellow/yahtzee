f052588898e686adc575cb6412df196a647ee41f
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-15 22:55:21 +0300

    fix rerolling when there are no more rolls left

--- src/Game.js
+++ src/Game.js
@@ -45,13 +45,15 @@ class Game extends Component {
   }
 
   toggleLocked(idx) {
-    this.setState((st) => ({
-      locked: [
-        ...st.locked.slice(0, idx),
-        !st.locked[idx],
-        ...st.locked.slice(idx + 1),
-      ],
-    }));
+    if (!this.state.locked.every((x) => x)) {
+      this.setState((st) => ({
+        locked: [
+          ...st.locked.slice(0, idx),
+          !st.locked[idx],
+          ...st.locked.slice(idx + 1),
+        ],
+      }));
+    }
   }
 
   doScore(rulename, ruleFn) {
